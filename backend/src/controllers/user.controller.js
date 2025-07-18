88b2e97202b670f2396ec8b8d39603631db4c62e
dhruv086 <dhruvagarwal086@gmail.com>
2025-07-18 08:04:54 +0530

    fix: update cookie settings for improved security and compatibility

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -14,8 +14,8 @@ const generateToken = (userId,res)=>{
   res.cookie("token",token,{
     maxAge:7*24*60*60*1000,
     httpOnly:true,
-    sameSite:"strict",
-    secure:process.env.NODE_ENV !== "development",
+    sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+    secure: process.env.NODE_ENV !== "development",
   })
 
   return  token
